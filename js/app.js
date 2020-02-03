adcf39e7abb2eb70ad1102256dad61fa298f9726
Joseph Hammett <joseph.hammett99@gmail.com>
2020-02-03 09:56:19 -0600

    cleaning code

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@
 ////////////////////////////////BATTLE LOGIC///////////////////////////////////
 //var yourMove;
 var enemyMove;
-var enemyMove2;
 var savedEnemyMove;
-var savedEnemyMove2;
 var health = 100; //player starting health
 var enemyHealth = 100; //enemy starting health
 var rounds = 0;
@@ -21,209 +19,184 @@ var battleHUD = document.getElementsByClassName('battleHUD')[0]; //battle HUD
 battleHUD.style.display = 'none';
 let newgame = document.getElementById("start");
 let enable = 1;
-//let enable = 2;
-//newgame.addEventListener("click", begin);
 //
+
+
 function begin() {
 	document.getElementsByClassName("gameHUD")[0].style.display = "none";
 }
-//
+
+
 function enableButtons() {
 	attackButton.disabled = false;
 	counterButton.disabled = false;
 }
 
 
-
 //triggers the fight in the HTML
-	function startBattle(id) {
-		addRound();
-		enemyMove(id);
-		healthChange();
-		//healthChange2();
-		//gameOver();
+function startBattle(id) {
+	addRound();
+	enemyMove(id);
+	healthChange();
+	//healthChange2();
+	//gameOver();
+}
+//adds a round to the round counters
+function addRound() {
+	rounds += 1;
+}
+
+
+//adds the counter action to attack
+function counter(y) {
+	var move = Math.floor((Math.random() * 5));
+	if (move >= 3 && y === 'dropkick') {
+		speech = "Enemy counter successful! Took 10 damage";
+		health -= 10;
+	} else if (move >= 3 && y === 'counter') {
+		speech = "Player's counter successful! Enemy took 20 damage!";
+		enemyHealth -= 20;
+	} else if (move < 3 && y === 'dropkick') {
+		speech = "Counter failed! Dealt 15 damage";
+		enemyHealth -= 15;
+	} else if (move < 3 && y === 'counter') {
+		speech = "Player's counter failed! Dealt 15 damage";
+		health -= 15;
 	}
-	//adds a round to the round counters
-	function addRound() {
-		rounds += 1;
+
+}
+
+
+//dislpays results of the round
+function roundResults(speech) {
+	battleLog.innerHTML += speech + "<br>";
+
+	if (enemyHealth === 0) {
+		battleLog.innerHTML = ''
 	}
-	
-	//adds the counter action to attack
-	function counter(y) {
-		var move = Math.floor((Math.random()*5));
-		if (move >= 3 && y === 'dropkick') {
-			speech = "Enemy counter successful! Took 10 damage";
-			health -= 10;
-		} else if (move >= 3 && y === 'counter') {
-			speech = "Player's counter successful! Enemy took 20 damage!";
-			enemyHealth -= 20;
-		} else if (move < 3 && y === 'dropkick') {
-			speech = "Counter failed! Dealt 15 damage";
+}
+
+
+function healthChange() {
+	healthBar.style.width = health + "%";
+	enemyHealthBar.style.width = enemyHealth + "%";
+	if (enemyHealth === 0) {
+		if (enable == 1) {
+			scenario.six.buttons[0] = ["Next", "advanceTo(scenario.seven)"]
+			scenario.six.text = "Key Guardian was defeated!"
+			enemyHealth = 100;
+			enemyHealthBar.style.width = enemyHealth + "%"; //resets battle
+			// enable = false;
+			enable = 2;
+			requestAnimationFrame(advanceTo(scenario.six))
+		} else if (enable == 2) {
+			scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
+			scenario.oneFive.text = "..."
+			enemyHealth = 100;
+			enemyHealthBar.style.width = enemyHealth + "%"; //resets battle
+			// enable = false;
+			enable = 3;
+			requestAnimationFrame(advanceTo(scenario.oneFive))
+		} else if (enable == 3) {
+			scenario.oneEleven.buttons[0] = ["Next", "advanceTo(scenario.oneTwelve)"]
+			scenario.oneEleven.text = "Enemy Defeated!"
+			enemyHealth = 100;
+			enemyHealthBar.style.width = enemyHealth + "%"; //resets battle
+			enable = 4;
+			requestAnimationFrame(advanceTo(scenario.oneEleven))
+		}
+	} else if (health === 0) {
+		console.log('gameover');
+	}
+}
+
+
+//takes moves of the player, generates one for enemy then runs the damage step
+function enemyMove(id) {
+	var move = Math.floor((Math.random() * 4) + 1);
+	if (move <= 3) {
+		savedEnemyMove = 'dropkick';
+	} else {
+		savedEnemyMove = 'counter';
+	};
+	damageStep(id, savedEnemyMove);
+	roundResults(speech);
+
+}
+
+
+//proccesses the moves to a result
+function damageStep(y, c) {
+	if (y === 'dropkick' && c === 'dropkick') {
+		speech = 'Both parties took damage';
+		if (enemyHealth >= 15 && health >= 10) {
 			enemyHealth -= 15;
-		} else if (move < 3 && y === 'counter') {
-			speech = "Player's counter failed! Dealt 15 damage";
-			health -= 15;
-		}
-	
-	}
-	
-	//dislpays results of the round
-	function roundResults(speech) {
-		battleLog.innerHTML += speech + "<br>";
-	
-		if (enemyHealth === 0) {
-			battleLog.innerHTML = ''
-		}
-	}
-	
-	function healthChange() {
-		healthBar.style.width = health + "%";
-		enemyHealthBar.style.width =  enemyHealth + "%";
-			if(enemyHealth === 0) {
-				if (enable == 1) {
-					scenario.six.buttons[0] = ["Next", "advanceTo(scenario.seven)"]
-					scenario.six.text = "Key Guardian was defeated!"
-					//scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
-					enemyHealth = 100;
-					enemyHealthBar.style.width =  enemyHealth + "%"; //resets battle
-					// enable = false;
-					enable = 2;
-					requestAnimationFrame(advanceTo(scenario.six))
-				} else if (enable == 2) {
-					scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
-					scenario.oneFive.text = "..."
-					//scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
-					enemyHealth = 100;
-					enemyHealthBar.style.width =  enemyHealth + "%"; //resets battle
-					// enable = false;
-					enable = 3;
-					requestAnimationFrame(advanceTo(scenario.oneFive))
-				} else if (enable == 3) {
-					scenario.oneEleven.buttons[0] = ["Next", "advanceTo(scenario.oneTwelve)"]
-					scenario.oneEleven.text = "Enemy Defeated!"
-					//scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
-					enemyHealth = 100;
-					enemyHealthBar.style.width =  enemyHealth + "%"; //resets battle
-					enable = 4;
-					requestAnimationFrame(advanceTo(scenario.oneEleven))
-				} 
-
-
-
-			
-				// enemyHealth = 100;
-				// enemyHealthBar.style.width =  enemyHealth + "%";
-		
-			} else if (health === 0) {
-				console.log('gameover');
-			}
-
-	}
-
-	
-	/*function healthChange2() {
-		healthBar.style.width = health + "%";
-		enemyHealthBar.style.width =  enemyHealth + "%";
-			if(enemyHealth === 0) {
-				if (enable2 == 1) {
-				scenario.twoThree.buttons[0] = ["Next", "advanceTo(scenario.twoFour)"]
-				//scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
-				enemyHealth = 100;
-				enemyHealthBar.style.width =  enemyHealth + "%"; //resets battle
-				enable = false;
-					requestAnimationFrame(advanceTo(scenario.twoThree))
-				}}}*/
-	
-	//takes moves of the player, generates one for enemy then runs the damage step
-	function enemyMove(id) {
-		var move = Math.floor((Math.random()*4)+1);
-		if (move <= 3) {
-			savedEnemyMove =  'dropkick';
+			health -= 10;
 		} else {
-			savedEnemyMove = 'counter';
-		};
-		damageStep(id, savedEnemyMove);
-		roundResults(speech);
-	
-	}
-	
-	//proccesses the moves to a result
-	function damageStep(y, c) {
-		if ( y === 'dropkick' && c === 'dropkick') {
-			speech = 'Both parties took damage';
-			if (enemyHealth >= 15 && health >= 10) {
-				enemyHealth -= 15;
-				health -= 10;
-			} else {
-				enemyHealth = 0;
-				health = 100;
-			}
-		} else if ( y === 'counter' && c === 'counter') {
-			speech = 'You defend yourself';
-		} else if ( y === 'dropkick' && c === 'counter') {
-			speech = 'Enemy readies itself';
-			counter(y, c);
-		} else if ( y === 'counter' && c === 'dropkick') {
-			speech = 'You ready yourself';
-			counter(y, c);
+			enemyHealth = 0;
+			health = 100;
 		}
+	} else if (y === 'counter' && c === 'counter') {
+		speech = 'You defend yourself';
+	} else if (y === 'dropkick' && c === 'counter') {
+		speech = 'Enemy readies itself';
+		counter(y, c);
+	} else if (y === 'counter' && c === 'dropkick') {
+		speech = 'You ready yourself';
+		counter(y, c);
 	}
+}
 
 
+window.onload = enableButtons();
 
 
-window.onload=enableButtons();
+// function startBattle2(id) {
+// 	addRound2();
+// 	enemyMove2(id);
+// 	healthChange2();
+// 	//healthChange2();
+// 	//gameOver();
+// }
+// function addRound2() {
+// 	rounds += 1;
+// }
+// function enemyMove2(id) {
+// 	var move = Math.floor((Math.random()*4)+1);
+// 	if (move <= 3) {
+// 		savedEnemyMove2 =  'dropkick';
+// 	} else {
+// 		savedEnemyMove2 = 'counter';
+// 	};
+// 	damageStep(id, savedEnemyMove2);
+// 	roundResults(speech);
 
+// }
+// function healthChange2() {
+// 	healthBar.style.width = health + "%";
+// 	enemyHealthBar.style.width =  enemyHealth + "%";
+// 	if(enemyHealth === 0) {
+// 		scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
+// 		//scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
+// 		enemyHealth = 100;
+// 		enemyHealthBar.style.width =  enemyHealth + "%"; //resets battle
+// 		requestAnimationFrame(advanceTo(scenario.oneFive))
+// 		//requestAnimationFrame(advanceTo(scenario.oneFive))
 
+// 		// enemyHealth = 100;
+// 		// enemyHealthBar.style.width =  enemyHealth + "%";
 
-	// function startBattle2(id) {
-	// 	addRound2();
-	// 	enemyMove2(id);
-	// 	healthChange2();
-	// 	//healthChange2();
-	// 	//gameOver();
-	// }
-	// function addRound2() {
-	// 	rounds += 1;
-	// }
-	// function enemyMove2(id) {
-	// 	var move = Math.floor((Math.random()*4)+1);
-	// 	if (move <= 3) {
-	// 		savedEnemyMove2 =  'dropkick';
-	// 	} else {
-	// 		savedEnemyMove2 = 'counter';
-	// 	};
-	// 	damageStep(id, savedEnemyMove2);
-	// 	roundResults(speech);
-	
-	// }
-	// function healthChange2() {
-	// 	healthBar.style.width = health + "%";
-	// 	enemyHealthBar.style.width =  enemyHealth + "%";
-	// 	if(enemyHealth === 0) {
-	// 		scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
-	// 		//scenario.oneFive.buttons[0] = ["Next", "advanceTo(scenario.oneSix)"]
-	// 		enemyHealth = 100;
-	// 		enemyHealthBar.style.width =  enemyHealth + "%"; //resets battle
-	// 		requestAnimationFrame(advanceTo(scenario.oneFive))
-	// 		//requestAnimationFrame(advanceTo(scenario.oneFive))
-	
-	// 		// enemyHealth = 100;
-	// 		// enemyHealthBar.style.width =  enemyHealth + "%";
-	
-	// 	} else if (health === 0) {
-	// 		console.log('gameover');
-	// 	}
-	// }
-
-
+// 	} else if (health === 0) {
+// 		console.log('gameover');
+// 	}
+// }
 
 
 /////////////////////////////////////GAME LOGIC////////////////////////////////////////
 
 
-var images = document.getElementById("images"); 
-var text = document.getElementById("text"); 
+var images = document.getElementById("images");
+var text = document.getElementById("text");
 var buttonBox = document.getElementById('buttonBox');
 var input = document.getElementById('input');
 //variable for the name of the character
@@ -231,51 +204,49 @@ var protag;
 
 
 //name scenario starts, advances to next scenario on Enter
-input.onkeypress = function(event) {
-  console.log(input.value);
-  if (event.key == "Enter" || event.keyCode == 13) { //keycode '13' equal to 'Enter' key
-    protag =  input.value;
-    input.parentNode.removeChild(input) //removes the input bar after first scenario
-    advanceTo(scenario.two)
-  }
+input.onkeypress = function (event) {
+	console.log(input.value);
+	if (event.key == "Enter" || event.keyCode == 13) { //keycode '13' equal to 'Enter' key
+		protag = input.value;
+		input.parentNode.removeChild(input) //removes the input bar after first scenario
+		advanceTo(scenario.two)
+	}
 };
 
 //replaces 'Player' with your input
-var changeText = function(words) {
-  text.innerHTML = words.replace("Player", protag);
+var changeText = function (words) {
+	text.innerHTML = words.replace("Player", protag);
 };
 
-var changeImage = function(img) {
-  images.style.backgroundImage = "url(" + img + ")";
+var changeImage = function (img) {
+	images.style.backgroundImage = "url(" + img + ")";
 };
 
-var changeButtons = function(buttonList) {
-  buttonBox.innerHTML = "";
-  for (var i = 0; i < buttonList.length; i++) {
-    buttonBox.innerHTML += "<button onClick="+buttonList[i][1]+">" + buttonList[i][0] + "</button>";
-  };
+var changeButtons = function (buttonList) {
+	buttonBox.innerHTML = "";
+	for (var i = 0; i < buttonList.length; i++) {
+		buttonBox.innerHTML += "<button onClick=" + buttonList[i][1] + ">" + buttonList[i][0] + "</button>";
+	};
 };
 
 //displays battle HUD in specific scenarios
 var showBattleHUD = function (yesorno) {
-	
-	if(yesorno === 'yes') {
+
+	if (yesorno === 'yes') {
 		battleHUD.style.display = 'initial';
-	} 
-	if (yesorno === 'no'){
+	}
+	if (yesorno === 'no') {
 		battleHUD.style.display = 'none';
-	} 
+	}
 }
 
-
 //moves the game along
-var advanceTo = function(s) {
-  changeImage(s.image)
-  changeText(s.text)
-  changeButtons(s.buttons)
-  showBattleHUD(s.test)
+var advanceTo = function (s) {
+	changeImage(s.image)
+	changeText(s.text)
+	changeButtons(s.buttons)
+	showBattleHUD(s.test)
 };
-
 
 
 //Text nodes
@@ -305,7 +276,7 @@ var scenario = {
 	six: {
 		image: src = "media/goatpxl.png",
 		text: "The Key Guardian attacks!",
-		buttons: [], //[]
+		buttons: [],
 		test: 'yes'
 	},
 	seven: {
@@ -363,7 +334,7 @@ var scenario = {
 		image: src = "media/cyberpxl.png",
 		text: "VROOM VROOM",
 		test: 'yes',
-		buttons: []//[["Next", "advanceTo(scenario.oneTwelve)"]]
+		buttons: []
 	},
 	oneTwelve: {
 		text: "YOU WIN",
@@ -375,7 +346,7 @@ var scenario = {
 	},
 	twoOne: {
 		text: "You make your way towards the buffet stand. A man holding an overloaded hibachi plate spots you. What will you do?",
-		buttons: [["Run", "advanceTo(scenario.twoTwo)"],["FIGHT", "advanceTo(scenario.twoThree)"]]
+		buttons: [["Run", "advanceTo(scenario.twoTwo)"], ["FIGHT", "advanceTo(scenario.twoThree)"]]
 	},
 	twoTwo: {
 		text: "You try to run away, but the man catches up and corners you!",
@@ -385,13 +356,13 @@ var scenario = {
 		image: src = "media/mrxxpxl.png",
 		text: "The man vigorously eats some rice...",
 		test: 'yes',
-		buttons: []//[["Next", "advanceTo(scenario.twoFour)"]]
+		buttons: []
 	},
 	twoFour: {
 		image: src = "media/glowpxl.png",
 		text: "You manage to make the man stumble, but the rice gives him power. You run away into the bathroom, and notice a blue glow among the darkness. What will you do?",
 		buttons: [["Approach", "advanceTo(scenario.twoFive)"]],
-		test:'no'
+		test: 'no'
 	},
 	twoFive: {
 		image: src = "media/glowpxl.png",
@@ -409,13 +380,13 @@ var scenario = {
 	twoEight: {
 		image: src = "media/mrxxpxl.png",
 		text: "The man is going in for seconds!",
-		test:'yes',
-		buttons:[["Next", "advanceTo(scenario.twoNine)"]]
+		test: 'yes',
+		buttons: [["Next", "advanceTo(scenario.twoNine)"]]
 	},
 	twoNine: {
 		text: "You dropkick the man in the stomach, causing the front door keys to drop from his shirt pocket. You grab the key to unlock the door and flee from the building.",
-		buttons:[["Continue", "advanceTo(scenario.twoTen)"]],
-		test:'no'
+		buttons: [["Continue", "advanceTo(scenario.twoTen)"]],
+		test: 'no'
 	},
 	twoTen: {
 		text: "You exit the building and find yourself in the parking lot. Suddenly, a sense of impending doom lurks over you...",
@@ -429,11 +400,11 @@ var scenario = {
 	},
 	twoTwelve: {
 		text: "YOU WIN",
-		test:'no',
+		test: 'no',
 		buttons: [["Next", "advanceTo(scenario.twoThirteen)"]]
 	},
 	twoThirteen: {
-		test:'no'
+		test: 'no'
 	}
 };
 
